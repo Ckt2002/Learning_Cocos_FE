bb9a2c0b53d61b9a302fa69389ed4ad390f1e739
Chung Khanh Toan <toan652002@gmail.com>
2026-04-16 15:43:00 +0700

    Update
    Add recover Mana

--- assets/Script/Exercise_2/Ex2_Character.js
+++ assets/Script/Exercise_2/Ex2_Character.js
@@ -6,25 +6,30 @@ cc.Class({
         nameLabel: cc.Label,
 
         maxMana: 100,
+        manaRecovering: 5,
+        manaConsuming: 8,
         manaBar: cc.ProgressBar,
 
         direction: 1,
         moveSpeed: 10,
         isFlip: false,
 
+        movingNode: cc.Node,
         spineNode: cc.Node,
+        timeRoRecoverMana: 1,
         timeToReduceMana: 1,
         targetLeftX: -600,
         targetRightX: 600,
     },
 
+    isRecovering: false,
     isMoving: false,
     currentTime: 0,
     currentMana: 0,
     currentTargetX: 0,
 
     onLoad() {
-        this.currentTargetX = this.node.x;
+        this.currentTargetX = this.movingNode.x;
         this.currentTime = 0;
         this.nameLabel.string = this.characterName;
         this.currentMana = this.maxMana;
@@ -34,16 +39,24 @@ cc.Class({
     },
 
     update(dt) {
-        if (this.isMoving && this.currentMana > 0) {
-            this.node.x += this.direction * this.moveSpeed * dt;
+        if (this.isMoving) {
+            this.movingNode.x += this.direction * this.moveSpeed * dt;
             this.currentTime += dt;
             if (this.currentTime >= this.timeToReduceMana) {
-                this.updateMana(-1);
+                this.updateMana(-this.manaConsuming);
                 this.currentTime = 0;
             }
         }
 
-        this.isMoving = !this.checkReachTargetPosition();
+        if (!this.isMoving && this.currentMana < this.maxMana) {
+            this.currentTime += dt;
+            if (this.currentTime >= this.timeToReduceMana) {
+                this.updateMana(this.manaRecovering);
+                this.currentTime = 0;
+            }
+        }
+
+        this.isMoving = this.currentMana > 0 && !this.checkReachTargetPosition();
     },
 
     updateMana(value) {
@@ -56,9 +69,9 @@ cc.Class({
 
     checkReachTargetPosition() {
         if (this.direction < 0) {
-            return this.node.x <= this.currentTargetX;
+            return this.movingNode.x <= this.currentTargetX;
         }
-        return this.node.x >= this.currentTargetX;
+        return this.movingNode.x >= this.currentTargetX;
     },
 
     flip() {
